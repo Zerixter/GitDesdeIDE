e37ccbc9308f099242ecee0a1b398f026506d900
Hamza <hsaddouki@gmail.com>
2017-10-16 23:37:42 +0200

    Update App.java

---
+++ src/net/hamzasaddouki/App.java
@@ -0,0 +1,10 @@
+package net.hamzasaddouki;
+
+public class App {
+    private static final int[] ARRAY = {1, 2, 3, 4, 5, 6};
+    public static void main(String[] args) {
+        for (int i = 0; i < ARRAY.length; i++) {
+            System.out.println(ARRAY[i]);
+        }
+    }
+}
